e097837f31ab3435a49d1fe259947bad13139311
David Germain <davidispizza@gmail.com>
2016-05-05 14:02:18 -0400

    renamed

--- SortMyCode/src/CodeSortTests.java
+++ SortMyCode/src/CodeSortTests.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import org.junit.Test;
 
-public class MainTests {
+public class CodeSortTests {
 
 	@Test
 	public void sort_putIncludeStatementFirst() {
