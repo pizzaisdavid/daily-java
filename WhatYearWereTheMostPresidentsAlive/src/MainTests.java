e490b16acab961ebadfc38abddb09b5a9b18090e
David Germain <davidispizza@gmail.com>
2016-05-06 11:57:05 -0400

    parse tests

---
+++ WhatYearWereTheMostPresidentsAlive/src/MainTests.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class MainTests {
+
+  @Test
+  public void test() {
+    String[] expected = {
+        "George Washington",
+        "Feb 22 1732",
+        "Westmoreland Co. Va.",
+        "Dec 14 1799",
+        "Mount Vernon Va."
+        };
+    String input = "George Washington,  Feb 22 1732,    Westmoreland Co. Va.,   Dec 14 1799,    Mount Vernon Va.";
+    String delimiter = ",";
+    ArrayList<String> output = Parser.parse(input, delimiter);
+    
+    assertEquals(Arrays.asList(expected), output);
+  }
+
+}
