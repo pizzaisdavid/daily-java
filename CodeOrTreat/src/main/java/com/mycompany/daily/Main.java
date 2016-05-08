4c744822052175ff16e8be1c9b4d37b04912ff64
David Germain <davidispizza@gmail.com>
2016-05-08 11:51:36 -0400

    no errors

--- CodeOrTreat/src/main/java/com/mycompany/daily/Main.java
+++ CodeOrTreat/src/main/java/com/mycompany/daily/Main.java
@@ -1,13 +1,16 @@
 package com.mycompany.daily;
-
+import main.java.com.mycompany.daily.Parser;
+import main.java.com.mycompany.daily.TextFile;
 /**
  * mvn compile  
  * mvn exec:java -Dexec.mainClass="com.mycompany.daily.Main"
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+  
+    public static void main( String[] args ) {
+      TextFile t = new TextFile("poop");
+      
+      System.out.println( "Hello World!" );
+        
     }
 }
