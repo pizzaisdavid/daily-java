993cbf3526ec1d06be52486a14f9099d3e7fde76
David Germain <davidispizza@gmail.com>
2016-05-09 18:10:47 -0400

    init

---
+++ BasicGraphStatisticsNodeDegrees/src/main/java/BasicGraphStatisticsNodeDegrees/BasicGraphStatisticsNodeDegrees/Main.java
@@ -0,0 +1,14 @@
+package BasicGraphStatisticsNodeDegrees.BasicGraphStatisticsNodeDegrees;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //test
+    }
+}
---
+++ BasicGraphStatisticsNodeDegrees/src/test/java/BasicGraphStatisticsNodeDegrees/BasicGraphStatisticsNodeDegrees/MainTest.java
@@ -0,0 +1,30 @@
+package BasicGraphStatisticsNodeDegrees.BasicGraphStatisticsNodeDegrees;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MainTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
