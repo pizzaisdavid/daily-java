4fa08c83b9b9a00e402f33eaacff123d8995664f
David Germain <davidispizza@gmail.com>
2016-05-09 20:13:32 -0400

    fixed file path

---
+++ BasicGraphStatisticsNodeDegrees/src/test/java/com/mycompany/daily/GraphTest.java
@@ -0,0 +1,17 @@
+package BasicGraphStatisticsNodeDegrees.BasicGraphStatisticsNodeDegrees;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import BasicGraphStatisticsNodeDegrees.BasicGraphStatisticsNodeDegrees.Graph;
+
+public class GraphTest {
+  
+
+  @Test
+  public void rtrdtd() {
+    assertEquals(true, true);
+  }
+ 
+}
