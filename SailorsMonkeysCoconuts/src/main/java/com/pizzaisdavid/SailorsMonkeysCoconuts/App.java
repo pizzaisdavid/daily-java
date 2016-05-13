5d243c5e49756fe923644f95840a381019b2035b
David Germain <davidispizza@gmail.com>
2016-05-12 23:40:48 -0400

    pass the main test

--- SailorsMonkeysCoconuts/src/main/java/com/pizzaisdavid/SailorsMonkeysCoconuts/App.java
+++ SailorsMonkeysCoconuts/src/main/java/com/pizzaisdavid/SailorsMonkeysCoconuts/App.java
@@ -1,13 +1,42 @@
 package com.pizzaisdavid.SailorsMonkeysCoconuts;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("");
+    }
+    
+    public static int getStartingCoconutCount(int numberOfSailors) {
+      double multiplier = calculateFairShare(numberOfSailors);
+      int countAfterWakingUp = 2;
+      while (true) {
+        double countBeforeGoingToSleep = (double) countAfterWakingUp;
+        for (int i = 0; i <= numberOfSailors; i++) {
+          if (i == numberOfSailors) {
+            return (int) countBeforeGoingToSleep;
+          } 
+          countBeforeGoingToSleep *= multiplier;
+          if (isWholeNumber(countBeforeGoingToSleep)) {
+            countBeforeGoingToSleep = payMonkeyTax(countBeforeGoingToSleep);
+          } else {
+            countAfterWakingUp++;
+            break;
+          }
+        }
+      }
+    }
+    
+    public static double calculateFairShare(int sailors) {
+      return (double) sailors / (double) (sailors - 1);
+    }
+    
+    public static boolean isWholeNumber(double d) {
+      return (d % 1) == 0;
+    }
+    
+    public static double payMonkeyTax(double d) {
+      final int AMOUNT_TO_PAY_MONKEY = 1;
+      return d + AMOUNT_TO_PAY_MONKEY;
     }
 }
