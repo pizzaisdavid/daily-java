57369381fbdc7bc2d3b675aa0d6fa668b84791ef
David Germain <davidispizza@gmail.com>
2016-05-15 11:51:54 -0400

    refined tests

--- SortMyCode/src/CodeSortTests.java
+++ SortMyCode/src/CodeSortTests.java
@@ -2,51 +2,40 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import org.junit.Test;
 
+import test.java.com.mycompany.daily.JunitHelper;
+
 public class CodeSortTests {
 
 	@Test
 	public void sort_putIncludeStatementFirst() {
-		ArrayList<String> program = new ArrayList<String>();
-		program.add("}");
-		program.add("#include <iostream>");
-		
-		CodeSort x = new CodeSort(program);
-		ArrayList<String> output = x.sort();
-		
-		assertEquals("#include <iostream>", output.get(0));
-		assertEquals("}", output.get(1));
-		assertEquals(2, output.size());
+		ArrayList<String> expected = JunitHelper.make("#include <iostream>", "}");
+    ArrayList<String> program = JunitHelper.make("}", "#include <iostream>");
+		CodeSort code = new CodeSort(program);
+		ArrayList<String> output = code.sort();
+		assertEquals(expected, output);
 	}
 	
 	@Test
 	public void sort_initializeVariablesBeforeUse() {
-	  ArrayList<String> program = new ArrayList<String>();
-	  program.add("sum + i = sum;");
-	  program.add("var sum = 0;");
-	  
-	  CodeSort x = new CodeSort(program);
-	  ArrayList<String> output = x.sort();
-	  
-	  assertEquals("var sum = 0;", output.get(0));
-	  assertEquals("sum + i = sum;", output.get(1));
-	  assertEquals(2, output.size());
+	  ArrayList<String> expected = JunitHelper.make("var sum = 0;", "sum + i = sum;");
+	  ArrayList<String> program = JunitHelper.make("sum + i = sum;", "var sum = 0;");
+	  CodeSort code = new CodeSort(program);
+	  ArrayList<String> output = code.sort();
+	  assertEquals(expected, output);
 	}
 	
-	 @Test
-	  public void sort() {
-	    ArrayList<String> program = new ArrayList<String>();
-	    program.add("sum + i = sum;");
-	    program.add("var sum = 0;");
-	    program.add("}");
-	    program.add("#include <iostream>");
-	    
-	    CodeSort x = new CodeSort(program);
-	    ArrayList<String> output = x.sort();
-	    
-	    assertEquals("#include <iostream>", output.get(0));
-	    assertEquals("var sum = 0;", output.get(1));
-	    assertEquals(4, output.size());
-	  }
-	
-
+	@Test
+  public void sort() {
+    ArrayList<String> program = JunitHelper.make(
+      "sum + i = sum;",
+      "var sum = 0;",
+      "}",
+      "#include <iostream>"
+    );
+    CodeSort code = new CodeSort(program);
+    ArrayList<String> output = code.sort();
+    assertEquals("#include <iostream>", output.get(0));
+    assertEquals("var sum = 0;", output.get(1));
+    assertEquals(4, output.size());
+  }
 }
