967c3c84264b50d054240e77843cb6d8ac9b5b36
David Germain <davidispizza@gmail.com>
2016-05-14 21:30:51 -0400

    total to amount

--- SailorsMonkeysCoconuts/src/main/java/com/pizzaisdavid/SailorsMonkeysCoconuts/Sailor.java
+++ SailorsMonkeysCoconuts/src/main/java/com/pizzaisdavid/SailorsMonkeysCoconuts/Sailor.java
@@ -3,17 +3,22 @@ package com.pizzaisdavid.SailorsMonkeysCoconuts;
 public class Sailor {
 
   private double fairShareRatio;
+  private int count;
 
   public Sailor(int sailorCount) {
     final int INDIVIDUAL_PERSON = 1;
+    this.count = sailorCount;
     this.fairShareRatio = (double) INDIVIDUAL_PERSON / (double) sailorCount;
   }
   
   public void takeFairShare(CoconutPile coconutPile) {
-    double total = coconutPile.getTotal();
+    double total = coconutPile.getAmount();
     double sailorsFairShare = total * this.fairShareRatio;
     double remainingAmount = total - sailorsFairShare;
-    coconutPile.setTotal(remainingAmount);
+    coconutPile.setAmount(remainingAmount);
   }
-
+  
+  public int getCount() {
+    return this.count;
+  }
 }
