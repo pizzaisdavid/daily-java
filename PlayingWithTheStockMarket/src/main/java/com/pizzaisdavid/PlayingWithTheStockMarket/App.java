1d701c999ab7c647d40281ae97346d058f265de9
David Germain <davidispizza@gmail.com>
2016-05-16 21:55:55 -0400

    works

--- PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/App.java
+++ PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/App.java
@@ -4,28 +4,36 @@ import java.util.ArrayList;
 
 import main.java.com.mycompany.daily.Parser;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println(getStocks("19.35 19.30 18.88 18.93 18.95 19.03 19.00 18.97 18.97 18.98"));
-    }
-    
-    public static String getStocks(String rawInput) {
-      ArrayList<Double> prices = parsePrices(rawInput);
-      return "temp";
-    }
-    
-    public static ArrayList<Double> parsePrices(String rawInput) {
-      ArrayList<Double> prices = new ArrayList<Double>();
-      for (String number: Parser.parse(rawInput, " ")) {
-        prices.add(new Double(number));
+  public static void main( String[] args )
+  {
+    Transaction transaction = getStocks("19.35 19.30 18.88 18.93 18.95 19.03 19.00 18.97 18.97 18.98");
+      System.out.println(transaction);
+  }
+  
+  public static Transaction getStocks(String rawInput) {
+    ArrayList<Double> prices = parsePrices(rawInput);
+    Transaction highestProfit = null;
+    for (Double buyPrice : prices) {
+      for (Double sellPrice : prices) {
+        Transaction transaction = new Transaction(buyPrice, sellPrice);
+        if (highestProfit == null || transaction.profit > highestProfit.profit) {
+          highestProfit = transaction;
+        }
       }
-      return prices;
     }
+    return highestProfit;
+  }
+  
+  public static ArrayList<Double> parsePrices(String rawInput) {
+    ArrayList<Double> prices = new ArrayList<Double>();
+    for (String number: Parser.parse(rawInput, " ")) {
+      prices.add(new Double(number));
+    }
+    return prices;
+  }
 
 }
+
+
---
+++ PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/Transaction.java
@@ -0,0 +1,21 @@
+package com.pizzaisdavid.PlayingWithTheStockMarket;
+
+public class Transaction {
+  public double buyPrice;
+  public double sellPrice;
+  public double profit;
+  
+  public Transaction(double buyPrice, double sellPrice) {
+    this.buyPrice = buyPrice;
+    this.sellPrice = sellPrice;
+    this.profit = calculateProfit(buyPrice, sellPrice);
+  }
+  
+  public double calculateProfit(double buyPrice, double sellPrice) {
+    return sellPrice - buyPrice;
+  }
+  
+  public String toString() {
+    return this.buyPrice + " " + this.sellPrice;
+  }
+}
