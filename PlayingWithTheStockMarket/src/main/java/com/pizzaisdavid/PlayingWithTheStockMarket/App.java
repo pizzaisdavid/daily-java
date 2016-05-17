3f95b206218ae03cc1874991761bd3c46f32b045
David Germain <davidispizza@gmail.com>
2016-05-16 22:39:13 -0400

    better variable name

--- PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/App.java
+++ PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/App.java
@@ -8,15 +8,18 @@ public class App
 {
   public static void main( String[] args )
   {
-    Transaction transaction = getStocks("19.35 19.30 18.88 18.93 18.95 19.03 19.00 18.97 18.97 18.98");
+    Transaction transaction = getBestStockTrade("19.35 19.30 18.88 18.93 18.95 19.03 19.00 18.97 18.97 18.98");
       System.out.println(transaction);
   }
   
-  public static Transaction getStocks(String rawInput) {
+  public static Transaction getBestStockTrade(String rawInput) {
     ArrayList<Double> prices = parsePrices(rawInput);
     Transaction highestProfit = null;
-    for (Double buyPrice : prices) {
-      for (Double sellPrice : prices) {
+    int length = prices.size();
+    for (int i = 0; i < length; i++) {
+      double buyPrice = prices.get(i);
+      for (int k = i; k < length; k++) {
+        double sellPrice = prices.get(k);
         Transaction transaction = new Transaction(buyPrice, sellPrice);
         if (highestProfit == null || transaction.profit > highestProfit.profit) {
           highestProfit = transaction;
