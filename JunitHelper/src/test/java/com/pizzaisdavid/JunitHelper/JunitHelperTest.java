9743d248552b00fae120f31e718aa1c6a36f3ba3
David Germain <davidispizza@gmail.com>
2016-05-16 23:11:45 -0400

    added Junit 4 to build path

---
+++ JunitHelper/src/test/java/com/pizzaisdavid/JunitHelper/JunitHelperTest.java
@@ -0,0 +1,14 @@
+package com.pizzaisdavid.JunitHelper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitHelperTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
