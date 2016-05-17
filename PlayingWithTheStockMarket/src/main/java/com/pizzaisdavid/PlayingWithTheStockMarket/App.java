67d3d47109f3d421e12437069efa7d2c6cc995d8
David Germain <davidispizza@gmail.com>
2016-05-16 21:18:38 -0400

    added link to project

--- PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/App.java
+++ PlayingWithTheStockMarket/src/main/java/com/pizzaisdavid/PlayingWithTheStockMarket/App.java
@@ -1,5 +1,9 @@
 package com.pizzaisdavid.PlayingWithTheStockMarket;
 
+import java.util.ArrayList;
+
+import main.java.com.mycompany.daily.Parser;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(getStocks("19.35 19.30 18.88 18.93 18.95 19.03 19.00 18.97 18.97 18.98"));
     }
+    
+    public static String getStocks(String rawInput) {
+      ArrayList<Double> prices = parsePrices(rawInput);
+      return "temp";
+    }
+    
+    public static ArrayList<Double> parsePrices(String rawInput) {
+      ArrayList<Double> prices = new ArrayList<Double>();
+      for (String number: Parser.parse(rawInput, " ")) {
+        prices.add(new Double(number));
+      }
+      return prices;
+    }
+
 }
