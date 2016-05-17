9c297e0288c632cfb1d83b5b76fdde80b0d50c21
David Germain <davidispizza@gmail.com>
2016-05-16 21:18:51 -0400

    Junit 4

--- PlayingWithTheStockMarket/src/test/java/com/pizzaisdavid/PlayingWithTheStockMarket/AppTest.java
+++ PlayingWithTheStockMarket/src/test/java/com/pizzaisdavid/PlayingWithTheStockMarket/AppTest.java
@@ -1,38 +1,19 @@
 package com.pizzaisdavid.PlayingWithTheStockMarket;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.util.ArrayList;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+import com.pizzaisdavid.JunitHelper.JunitHelper;
+
+public class AppTest {
+
+  @Test
+  public void parsePricesValidInput() {
+    ArrayList<Double> expected = JunitHelper.arrayList.make(2.0, 3.0, 4.0);
+    assertEquals(expected, App.parsePrices("2.0 3.0 4.0"));
+  }
+
 }
