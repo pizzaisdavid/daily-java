6409ab330c6ab7ddcae583c363d9509848d614bb
David Germain <davidispizza@gmail.com>
2016-05-20 12:54:29 -0400

    first try

--- AllThePlacesYourDogDidntWin/src/main/java/com/pizzaisdavid/AllThePlacesYourDogDidntWin/App.java
+++ AllThePlacesYourDogDidntWin/src/main/java/com/pizzaisdavid/AllThePlacesYourDogDidntWin/App.java
@@ -1,13 +1,37 @@
 package com.pizzaisdavid.AllThePlacesYourDogDidntWin;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(computeThePlacesYourDogDidNotWin(1));
+    }
+    
+    public static ArrayList<String> computeThePlacesYourDogDidNotWin(int place) {
+      ArrayList<String> placesNotWon = new ArrayList<String>();
+      for (int i = 1; i < 100; i++) {
+        if (i == place) {
+          // do nothing
+        } else {
+          placesNotWon.add(formatWithModifier(i));
+        }
+      }
+      return placesNotWon;
+    }
+
+    private static String formatWithModifier(int place) {
+      String modifier = "th";
+      if (place % 100 == 11 || place % 100 == 12 || place % 100 == 13) {
+        modifier = "th";
+      } else if (place % 10 == 1) {
+        modifier = "st";
+      } else if (place % 10 == 2) {
+        modifier = "nd";
+      } else if (place % 10 == 3) {
+        modifier = "rd";
+      }
+      return place + modifier;
     }
 }
