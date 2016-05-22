d59082bc4907869f4aabe3c3af57f876b85e905a
David Germain <davidispizza@gmail.com>
2016-05-22 18:24:18 -0400

    moved stuff to class

---
+++ pocketTest/src/main/java/pizzaisdavid/com/pocketTest/RequestToken.java
@@ -0,0 +1,48 @@
+package pizzaisdavid.com.pocketTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import pizzaisdavid.com.pocketTest.ConsumerKey;
+
+public class RequestToken {
+  
+  public RequestToken() {
+    
+  }
+  
+  public String get() throws Exception {
+    String url = "https://getpocket.com/v3/oauth/request";
+    URL obj = new URL(url);
+    HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
+
+    connection.setDoOutput(true);
+    connection.setDoInput(true);
+    connection.setRequestMethod("POST");
+    connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+    connection.setRequestProperty("X-Accept", "application/x-www-form-urlencoded");
+
+    String payload="{\"consumer_key\":\""
+    + ConsumerKey.getMac() 
+    + "\",\"redirect_uri\":\"https://www.google.com\"}";
+
+    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+    writer.write(payload);
+    writer.close();
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
+    }
+    in.close();
+    
+    return response.toString();
+  }
+}
