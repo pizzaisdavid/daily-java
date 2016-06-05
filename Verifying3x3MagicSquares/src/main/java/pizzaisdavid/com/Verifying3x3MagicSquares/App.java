b2f6c41b02e6267cbd120fbd2c5c5558a66849c2
David Germain <davidispizza@gmail.com>
2016-06-05 19:57:11 -0400

    initialize

---
+++ Verifying3x3MagicSquares/src/main/java/pizzaisdavid/com/Verifying3x3MagicSquares/App.java
@@ -0,0 +1,13 @@
+package pizzaisdavid.com.Verifying3x3MagicSquares;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
