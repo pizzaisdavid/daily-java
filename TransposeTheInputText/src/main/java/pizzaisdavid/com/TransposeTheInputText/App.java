9f6dd5ab8b697e9d59c85b816af80484be813874
David Germain <davidispizza@gmail.com>
2016-06-09 18:32:21 -0400

    started tests

--- TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/App.java
+++ TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/App.java
@@ -1,5 +1,8 @@
 package pizzaisdavid.com.TransposeTheInputText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -10,4 +13,32 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static Object transpose(List<String> table) {
+      List<String> matrix = new ArrayList<String>();
+      int newNumberOfRows = getLengthOfLongestRow(table);
+      for (int i = 0; i < table.size(); i++) {
+        
+      }
+      return matrix;
+    }
+
+    private static int getLengthOfLongestRow(List<String> table) {
+      List<Integer> lengths = new ArrayList<Integer>();
+      for (String row : table) {
+        lengths.add(row.length());
+      }
+      return max(lengths);
+    }
+
+    private static int max(List<Integer> values) {
+      int highest = values.get(0);
+      for (int value : values) {
+        if (value > highest) {
+          highest = value;
+        }
+      }
+      return highest;
+    }
+    
 }
