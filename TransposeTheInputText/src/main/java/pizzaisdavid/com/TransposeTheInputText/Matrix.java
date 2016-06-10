4c80352dda8162d2059be20f9e1a5b219599585c
David Germain <davidispizza@gmail.com>
2016-06-09 22:54:14 -0400

    break large function to smaller

--- TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/Matrix.java
+++ TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/Matrix.java
@@ -14,23 +14,39 @@ public class Matrix {
   }
 
   public void appendColumn(ArrayList<String> row) {
+    // TODO detect the need to make " " on its own. 
     for (int i = 0; i < rowLength_; i++) {
-      String entry = "";
-      try {
-        entry = row.get(i);
-      } catch (IndexOutOfBoundsException e) {
-        entry = " ";
-      }
-      try {
-        ArrayList<String> updatedRow = table_.get(i);
-        updatedRow.add(entry);
-        table_.set(i, updatedRow);
-      } catch (Exception e) {
-        ArrayList<String> newRow = new ArrayList<String>();
-        newRow.add(entry);
-        table_.add(newRow);
-      }
+      String entry = getEntryFromRowOrSpace(row, i);
+      appendToRow(entry, i);
     }
+  }
+  
+  private String getEntryFromRowOrSpace(ArrayList<String> row, int position) {
+    String entry = " ";
+    try {
+      entry = row.get(position);
+    } catch (IndexOutOfBoundsException e) {}
+    return entry;
+  }
+  
+  public void appendToRow(String entry, int position) {
+    try {
+      appendEntryToRow(entry, position);
+    } catch (Exception e) {
+      startNewColumn(entry);
+    }
+  }
+  
+  public void startNewColumn(String entry) {
+    ArrayList<String> newRow = new ArrayList<String>();
+    newRow.add(entry);
+    table_.add(newRow);
+  }
+  
+  public void appendEntryToRow(String entry, int position) throws Exception {
+    ArrayList<String> updatedRow = table_.get(position);
+    updatedRow.add(entry);
+    table_.set(position, updatedRow);
   }
   
   public List<ArrayList<String>> getContent() {
