eab02a7ca25d827167697fcbc6228779768a5d77
David Germain <davidispizza@gmail.com>
2016-06-15 22:57:51 -0400

    abstracting away details

--- CriticalHit/src/main/java/pizzaisdavid/com/CriticalHit/GameDice.java
+++ CriticalHit/src/main/java/pizzaisdavid/com/CriticalHit/GameDice.java
@@ -5,16 +5,13 @@ public class GameDice extends Dice {
   public GameDice(int sides) {
     super(sides);
   }
-
-  public boolean isCriticalHitRollNeeded(Enemy enemy) {
-    return enemy.getHealth() >= sides;
-  }
   
   public double computeCriticalHitProbability() {
     return computeRollProbability();
   }
   
   public int getCriticalHitValue() {
-    return sides;
+    return getHighestValue();
   }
+
 }
