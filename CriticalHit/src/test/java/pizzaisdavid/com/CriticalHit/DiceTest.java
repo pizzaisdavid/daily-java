5df980a150dacdf037ae686557b6996e817e515a
David Germain <davidispizza@gmail.com>
2016-06-15 23:33:07 -0400

    more parametrized tests

--- CriticalHit/src/test/java/pizzaisdavid/com/CriticalHit/DiceTest.java
+++ CriticalHit/src/test/java/pizzaisdavid/com/CriticalHit/DiceTest.java
@@ -2,43 +2,73 @@ package pizzaisdavid.com.CriticalHit;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 public class DiceTest {
   
-  @Test
-  public void computeProbabilityToRollAtLeast_100PercentChance() {
-    Dice die = new Dice(4);
-    assertEquals(1, die.computeProbabilityToRollAtLeast(1), 0.001);
+  @RunWith(Parameterized.class)
+  public static class computeProbabilityToRollAtLeast {
+    
+    @Parameters(name = "{0} chance")
+    public static Collection<Object[]> data() {
+      return Arrays.asList(new Object[][] {
+        {1.00, 1},
+        {0.75, 2},
+        {0.50, 3},
+        {0.25, 4}
+     });
+    }
+    
+    @Parameter(value = 0)
+    public double probability;
+    
+    @Parameter(value = 1)
+    public int minimumValue;
+    
+    @Test
+    public void test() {
+      Dice die = new Dice(4);
+      assertEquals(probability, die.computeProbabilityToRollAtLeast(minimumValue), 0.001);
+    }
   }
   
-  @Test
-  public void computeProbabilityToRollAtLeast_75PercentChance() {
-    Dice die = new Dice(4);
-    assertEquals(0.75, die.computeProbabilityToRollAtLeast(2), 0.001);
-  }
-  
-  @Test
-  public void computeProbabilityToRollAtLeast_50PercentChance() {
-    Dice die = new Dice(4);
-    assertEquals(0.50, die.computeProbabilityToRollAtLeast(3), 0.001);
-  }
-
-  @Test
-  public void computeProbabilityToRollAtLeast_25PercentChance() {
-    Dice die = new Dice(4);
-    assertEquals(0.25, die.computeProbabilityToRollAtLeast(4), 0.001);
-  }
-  
-  @Test
-  public void computeRollProbability_4SidedDice() {
-    Dice die = new Dice(4);
-    assertEquals(0.25, die.computeRollProbability(), 0.001);
-  }
-  
-  @Test
-  public void computeRollProbability_5SidedDice() {
-    Dice die = new Dice(5);
-    assertEquals(0.20, die.computeRollProbability(), 0.001);
+  @RunWith(Parameterized.class)
+  public static class computeRollProbability {
+    
+    @Parameters(name = "{0} sided die")
+    public static Collection<Object[]> data() {
+      return Arrays.asList(new Object[][] {
+        {4, 0.25},
+        {5, 0.20},
+        {6, 0.166666666}
+     });
+    }
+    
+    @Parameter(value = 0)
+    public int numberOfSides;
+    
+    @Parameter(value = 1)
+    public double probability;
+    
+    @Test
+    public void test() {
+      Dice die = new Dice(numberOfSides);
+      assertEquals(probability, die.computeRollProbability(), 0.001);
+    }
   }
 }
+
+//  
+//  @Test
+//  public void computeRollProbability_5SidedDice() {
+//    Dice die = new Dice(5);
+//    assertEquals(0.20, die.computeRollProbability(), 0.001);
+//  }
+//}
