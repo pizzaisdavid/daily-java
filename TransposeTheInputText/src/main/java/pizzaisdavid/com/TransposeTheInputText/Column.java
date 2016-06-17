68eff6595d3bad892312fe594cd9dafa05555c82
David Germain <davidispizza@gmail.com>
2016-06-16 20:06:22 -0400

    getCell function

--- TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/Column.java
+++ TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/Column.java
@@ -5,9 +5,18 @@ import java.util.ArrayList;
 public class Column extends ArrayList<String> {
 
   private static final long serialVersionUID = 1L;
+  private String CELl_PLACEHOLDER_VALUE = " ";
   
   public Column(ArrayList<String> row) {
     clear();
     addAll(row);
+  }
+  
+  public String getCell(int index) {
+    try {
+      return get(index);
+    } catch (IndexOutOfBoundsException e) {
+      return CELl_PLACEHOLDER_VALUE;
+    }
   }
 }
