6c6a242f0d1647dec3f7c759e22775d4539a3430
David Germain <davidispizza@gmail.com>
2016-06-16 20:02:37 -0400

    simplified column class

--- TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/Column.java
+++ TransposeTheInputText/src/main/java/pizzaisdavid/com/TransposeTheInputText/Column.java
@@ -2,28 +2,12 @@ package pizzaisdavid.com.TransposeTheInputText;
 
 import java.util.ArrayList;
 
-public class Column extends ArrayList<ArrayList<String>> {
+public class Column extends ArrayList<String> {
 
   private static final long serialVersionUID = 1L;
   
-  private Column(ArrayList<String> row) {
-    for (String cell: row) {
-      add(cell);
-    }
-  }
-
-  public static Column convertRowToColumn(ArrayList<String> row) {
-    return new Column(row);
-  }
-
-  public void add(String cell) {
-    ArrayList<String> singleCellRow = new ArrayList<String>();
-    singleCellRow.add(cell);
-    add(singleCellRow);
-  }
-  
-  public String get_(int index) {
-    int FIRST_VALUE = 0;
-    return get(index).get(FIRST_VALUE);
+  public Column(ArrayList<String> row) {
+    clear();
+    addAll(row);
   }
 }
