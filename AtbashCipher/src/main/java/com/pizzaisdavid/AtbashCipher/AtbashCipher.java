9a7045f81638c8ed0aac23d7d7d45ca6c6dfaad2
David Germain <davidispizza@gmail.com>
2016-06-17 08:16:32 -0400

    uses and if/else instead of try/catch

--- AtbashCipher/src/main/java/com/pizzaisdavid/AtbashCipher/AtbashCipher.java
+++ AtbashCipher/src/main/java/com/pizzaisdavid/AtbashCipher/AtbashCipher.java
@@ -21,17 +21,27 @@ public class AtbashCipher {
   
   private static char lookupCorrespondingCharacter(char character) {
     character = Character.toLowerCase(character);
-    try {
+    if (isAlphabetLetter(character)) {
       return atbashCipher(character);
-    } catch (StringIndexOutOfBoundsException e) {
-      return character;
+    } else {
+      return returnSymbol(character);
     }
   }
   
+  private static boolean isAlphabetLetter(char character) {
+    final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    return ALPHABET.indexOf(character) != -1;
+  }
+
   private static char atbashCipher(char letter) {
     final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     final String CHIPHER = "zyxwvutsrqponmlkjihgfedcba";
     int position = ALPHABET.indexOf(letter);
     return CHIPHER.charAt(position);
   }
+  
+  private static char returnSymbol(char character) {
+    return character;
+  }
+
 }
