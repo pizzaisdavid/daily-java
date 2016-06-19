bb87ce4569a89d468ff95cecf3fce8acd487bf3c
David Germain <davidispizza@gmail.com>
2016-06-19 19:31:02 -0400

    Merged classes

--- CodeOrTreat/src/main/java/com/mycompany/daily/StatisticsTracker.java
+++ CodeOrTreat/src/main/java/com/mycompany/daily/StatisticsTracker.java
@@ -1,37 +1,88 @@
 package com.mycompany.daily;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class StatisticsTracker {
-  private Frequency frequency;
-  private ArrayList<String> items;
+public class StatisticsTracker extends HashMap<String, Entry>{
+  private static final long serialVersionUID = 1L;
   private int total;
 
-  
-  public StatisticsTracker() {
-    frequency = new Frequency();
-  }
-
   public void compute(ArrayList<String> items) {
-    this.items = items;
     total = items.size();
-    setupEntries();
-  }
-
-  private void setupEntries() {
     for (String item: items) {
-      if (frequency.containsKey(item) == false) {
-        frequency.create(item);
+      if (containsKey(item) == false) {
+        create(item);
       }
-      frequency.increment(item);
-    }    
+      increment(item);
+    }       
+  }
+  
+  public void create(String item) {
+    put(item, Entry.withName(item));
   }
 
+  public void increment(String item) {
+    com.mycompany.daily.Entry entry = get(item);
+    entry.increment();
+    put(item, entry);    
+  }
+  
   public String toString() {
     String message = "";
-    for (Entry each : frequency.values()) {
+    for (Entry each : values()) {
       message += each.toString(total) + "\n";
     }
     return message;
+  }
+  
+  public int size() {
+    return super.size();
+  }
+
+  public boolean isEmpty() {
+    return super.isEmpty();
+  }
+
+  public boolean containsKey(Object key) {
+    return super.containsKey(key);
+  }
+
+  public boolean containsValue(Object value) {
+    return super.containsValue(value);
+  }
+
+  public com.mycompany.daily.Entry get(Object key) {
+    return super.get(key);
+  }
+
+  public com.mycompany.daily.Entry put(String key, com.mycompany.daily.Entry value) {
+    return super.put(key, value);
+  }
+
+  public com.mycompany.daily.Entry remove(Object key) {
+    return super.remove(key);
+  }
+
+  public void putAll(Map<? extends String, ? extends com.mycompany.daily.Entry> m) {
+    super.putAll(m);
+  }
+
+  public void clear() {
+    super.clear();
+  }
+
+  public Set<String> keySet() {
+    return super.keySet();
+  }
+
+  public Collection<com.mycompany.daily.Entry> values() {
+    return super.values();
+  }
+
+  public Set<java.util.Map.Entry<String, com.mycompany.daily.Entry>> entrySet() {
+    return super.entrySet();
   }
 }
