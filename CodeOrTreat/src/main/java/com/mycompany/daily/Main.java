fb4423b2a69cccd5c4b22a888e2365da2a0957df
David Germain <davidispizza@gmail.com>
2016-06-20 18:02:31 -0400

    Remove

--- CodeOrTreat/src/main/java/com/mycompany/daily/Main.java
+++ CodeOrTreat/src/main/java/com/mycompany/daily/Main.java
@@ -1,11 +1,15 @@
 package com.mycompany.daily;
+import java.util.ArrayList;
+
 import main.java.com.mycompany.daily.TextFile;
 
 public class Main {
   
     public static void main( String[] args ) {
       TextFile t = new TextFile("candy.txt");
-      CandyBag bag = new CandyBag(t.getLines());
-      System.out.println(bag.toString()); 
+      ArrayList<String> candies = t.getLines();
+      StatisticsTracker bag = new StatisticsTracker();
+      bag.compute(candies);
+      System.out.println(bag); 
     }
 }
