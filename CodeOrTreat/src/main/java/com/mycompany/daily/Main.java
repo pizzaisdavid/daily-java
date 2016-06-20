7f5ea0c93bddcd00263cf01d1f3efc174069101b
David Germain <davidispizza@gmail.com>
2016-06-20 18:16:43 -0400

    Make variable name more descriptive

--- CodeOrTreat/src/main/java/com/mycompany/daily/Main.java
+++ CodeOrTreat/src/main/java/com/mycompany/daily/Main.java
@@ -6,8 +6,8 @@ import main.java.com.mycompany.daily.TextFile;
 public class Main {
   
     public static void main( String[] args ) {
-      TextFile t = new TextFile("candy.txt");
-      ArrayList<String> candies = t.getLines();
+      TextFile file = new TextFile("candy.txt");
+      ArrayList<String> candies = file.getLines();
       StatisticsTracker bag = new StatisticsTracker();
       bag.compute(candies);
       System.out.println(bag); 
