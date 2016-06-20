9fe3a3fcf0153a28b278cd7c737afe0ad283b980
David Germain <davidispizza@gmail.com>
2016-06-19 20:50:38 -0400

    Fix tests

--- CodeOrTreat/src/test/java/com/mycompany/daily/CandyBayTest.java
+++ CodeOrTreat/src/test/java/com/mycompany/daily/CandyBayTest.java
@@ -1,62 +1,27 @@
 package com.mycompany.daily;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.Test;
 
-import junit.framework.TestCase;
 
-import org.junit.runner.RunWith;    
-import org.junit.runners.Suite;    
- 
-@RunWith(Suite.class)    
-@Suite.SuiteClasses({    
-  CandyBayTest.statisitcs.class,   
-  CandyBayTest.count.class   
-})
-
-public class CandyBayTest extends TestCase {
+public class CandyBayTest {
   
-  public static class statisitcs {
-
-    @Test
-    public void small() {
-      ArrayList<String> input = new ArrayList<String>(
-        Arrays.asList(
-          "red",
-          "blue",
-          "green",
-          "green"
-        )
-      );
-      Map<String, Integer> expected = new HashMap<String, Integer>(); 
-      expected.put("red", 1);
-      expected.put("blue", 1);
-      expected.put("green", 2);
-
-      CandyBag bag = new CandyBag(input);
-      //assertEquals(expected, bag.getStatisitcs());
-    }
-  }
-  
-  public static class count {
-
-    @Test
-    public void small() {
-      ArrayList<String> input = new ArrayList<String>(
-        Arrays.asList(
-          "red",
-          "blue",
-          "green",
-          "green"
-        )
-      );
-      int expected = 4;
-      CandyBag bag = new CandyBag(input);
-      //assertEquals(expected, bag.count());
-    }
+  @Test
+  public void small() {
+    ArrayList<String> input = new ArrayList<String>(
+      Arrays.asList(
+        "red",
+        "blue",
+        "green",
+        "green"
+      )
+    );
+    StatisticsTracker stats = new StatisticsTracker();
+    stats.compute(input);
+    assertEquals(3, stats.size());
   }
 }
