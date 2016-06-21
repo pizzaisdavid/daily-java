db2ba172d79457dbf85122fe940b03ed1c9c611a
David Germain <davidispizza@gmail.com>
2016-06-21 19:57:07 -0400

    Move logic to StatisticsTracker

--- WhatYearWereTheMostPresidentsAlive/src/Main.java
+++ WhatYearWereTheMostPresidentsAlive/src/Main.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Calendar;
-import main.java.com.mycompany.daily.Parser;
 import main.java.com.mycompany.daily.TextFile;
 
 public class Main {
@@ -10,24 +8,6 @@ public class Main {
     TextFile file = new TextFile("pres.txt");
     ArrayList<String> lines = file.getLines();
     StatisticsTracker tracker = new StatisticsTracker(lines);
-    
-    ArrayList<ArrayList<String>> parsed = Parser.parseExcludingHeader(lines);
-    ArrayList<Person> presidents = new ArrayList<Person>();
-    for (ArrayList<String> rawData : parsed) {
-      Person president = new Person(rawData.get(1), rawData.get(3));
-      presidents.add(president);
-    }
-    
-    for (int i = 1732; i < 2016; i++) {
-      int count = 0;
-      Calendar year = Calendar.getInstance();
-      year.set(Calendar.YEAR, i);
-      for (Person p : presidents) {
-        if (p.wasAliveDuring(year)) {
-          count++;
-        }
-      }
-      System.out.println("Year: " + i + ", count: " + count);
-    }
+    tracker.displayResults();
   }
 }
--- WhatYearWereTheMostPresidentsAlive/src/StatisticsTracker.java
+++ WhatYearWereTheMostPresidentsAlive/src/StatisticsTracker.java
@@ -1,8 +1,31 @@
 import java.util.ArrayList;
+import java.util.Calendar;
+
+import main.java.com.mycompany.daily.Parser;
 
 public class StatisticsTracker {
+  private ArrayList<Person> presidents;
 
   public StatisticsTracker(ArrayList<String> lines) {
+    ArrayList<ArrayList<String>> parsed = Parser.parseExcludingHeader(lines);
+    presidents = new ArrayList<Person>();
+    for (ArrayList<String> rawData : parsed) {
+      Person president = new Person(rawData.get(1), rawData.get(3));
+      presidents.add(president);
+    }
   }
 
+  public void displayResults() {
+    for (int i = 1732; i < 2016; i++) {
+      int count = 0;
+      Calendar year = Calendar.getInstance();
+      year.set(Calendar.YEAR, i);
+      for (Person p : presidents) {
+        if (p.wasAliveDuring(year)) {
+          count++;
+        }
+      }
+      System.out.println("Year: " + i + ", count: " + count);
+    }    
+  }
 }
