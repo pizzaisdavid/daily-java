406db9906c09167440307d60bd6fa945eeb34f06
David Germain <davidispizza@gmail.com>
2016-06-21 22:15:18 -0400

    Clean up class

--- WhatYearWereTheMostPresidentsAlive/src/StatisticsTracker.java
+++ WhatYearWereTheMostPresidentsAlive/src/StatisticsTracker.java
@@ -3,21 +3,24 @@ import java.util.Calendar;
 
 public class StatisticsTracker extends ArrayList<Person>{
   private static final long serialVersionUID = 1L;
-  private ArrayList<Person> presidents;
+  // TODO private int yearMostPresidentsWereAlive;
 
   public StatisticsTracker(ArrayList<ArrayList<String>> parsedPresidents) {
+    //yearMostPresidentsWereAlive = 0;
     for (ArrayList<String> each : parsedPresidents) {
       Person president = Person.parse(each);
       add(president);
+      // TODO update();
     }
   }
+
 
   public void displayResults() {
     for (int i = 1732; i < 2016; i++) {
       int count = 0;
       Calendar year = Calendar.getInstance();
       year.set(Calendar.YEAR, i);
-      for (Person p : presidents) {
+      for (Person p : this) {
         if (p.wasAliveDuring(year)) {
           count++;
         }
