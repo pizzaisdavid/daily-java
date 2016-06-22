3b7fa3e6c29ee009458161254adb4e3a0f59fb5d
David Germain <davidispizza@gmail.com>
2016-06-21 20:42:11 -0400

    Change variable back to presidents to lines.

--- WhatYearWereTheMostPresidentsAlive/src/Main.java
+++ WhatYearWereTheMostPresidentsAlive/src/Main.java
@@ -7,8 +7,8 @@ public class Main {
   
   public static void main(String[] args) {
     TextFile file = new TextFile("pres.txt");
-    ArrayList<String> presidents = file.getLines();
-    ArrayList<ArrayList<String>> parsedPresidents = Parser.parseExcludingHeader(presidents);
+    ArrayList<String> lines = file.getLines();
+    ArrayList<ArrayList<String>> parsedPresidents = Parser.parseExcludingHeader(lines);
     StatisticsTracker tracker = new StatisticsTracker(parsedPresidents);
     tracker.displayResults();
   }
