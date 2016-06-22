5302ddec1ba2d12d47ff4c79c19ee90f01fcf893
David Germain <davidispizza@gmail.com>
2016-06-21 20:24:08 -0400

    Change formatting

--- WhatYearWereTheMostPresidentsAlive/src/Main.java
+++ WhatYearWereTheMostPresidentsAlive/src/Main.java
@@ -4,7 +4,6 @@ import main.java.com.mycompany.daily.TextFile;
 public class Main {
   
   public static void main(String[] args) {
-    
     TextFile file = new TextFile("pres.txt");
     ArrayList<String> lines = file.getLines();
     StatisticsTracker tracker = new StatisticsTracker(lines);
