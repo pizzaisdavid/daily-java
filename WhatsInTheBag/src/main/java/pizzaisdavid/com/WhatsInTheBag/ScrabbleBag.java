7a44be1218c44750a6fecf2b807d1cc10da6dae3
David Germain <davidispizza@gmail.com>
2016-06-23 14:22:45 -0400

    Change String type to Character

--- WhatsInTheBag/src/main/java/pizzaisdavid/com/WhatsInTheBag/ScrabbleBag.java
+++ WhatsInTheBag/src/main/java/pizzaisdavid/com/WhatsInTheBag/ScrabbleBag.java
@@ -4,28 +4,28 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class ScrabbleBag extends HashMap<String, Integer> {
+public class ScrabbleBag extends HashMap<Character, Integer> {
   private static final long serialVersionUID = 1L;
 
   public ScrabbleBag() {
-    List<String> chars = Arrays.asList(
-        "A", "B", "C", "D", "E",
-        "F", "G", "H", "I", "J",
-        "K", "L", "M", "N", "O",
-        "P", "Q", "R", "S", "T",
-        "U", "V", "W", "X", "Y",
-        "Z", "_");
-    List<Integer> frequency = Arrays.asList(
+    List<Character> characters = Arrays.asList(
+        'A', 'B', 'C', 'D', 'E',
+        'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y',
+        'Z', '_');
+    List<Integer> frequencies = Arrays.asList(
         9, 2, 2, 4, 12,
         2, 3, 2, 9, 1,
         1, 4, 2, 6, 8,
         2, 1, 6, 4, 6,
         4, 2, 2, 1, 2,
         1, 2);
-    for (int i = 0; i < chars.size(); i++) {
-      String character = chars.get(i);
-      int occurrences = frequency.get(i);
-      put(character, occurrences);
+    for (int i = 0; i < characters.size(); i++) {
+      char character = characters.get(i);
+      int frequency = frequencies.get(i);
+      put(character, frequency);
     }
   }
 
