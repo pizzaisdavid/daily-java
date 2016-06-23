e00deb53d76a22319d5545179eb44f6176442fb3
David Germain <davidispizza@gmail.com>
2016-06-22 22:22:04 -0400

    Init

---
+++ WhatsInTheBag/src/main/java/pizzaisdavid/com/WhatsInTheBag/App.java
@@ -0,0 +1,13 @@
+package pizzaisdavid.com.WhatsInTheBag;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
