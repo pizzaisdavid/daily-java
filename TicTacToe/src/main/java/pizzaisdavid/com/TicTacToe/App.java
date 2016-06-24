e588d8918acc88eb55c524275229b2bd93cb1846
David Germain <davidispizza@gmail.com>
2016-06-24 15:56:34 -0400

    Init

---
+++ TicTacToe/src/main/java/pizzaisdavid/com/TicTacToe/App.java
@@ -0,0 +1,13 @@
+package pizzaisdavid.com.TicTacToe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
