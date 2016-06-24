52e25c0500416e5a572feb70c06691612a407c08
David Germain <davidispizza@gmail.com>
2016-06-24 16:18:48 -0400

    Check who's turn

--- TicTacToe/src/main/java/pizzaisdavid/com/TicTacToe/App.java
+++ TicTacToe/src/main/java/pizzaisdavid/com/TicTacToe/App.java
@@ -1,13 +1,9 @@
 package pizzaisdavid.com.TicTacToe;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(Game.isPlayerTurn("kid"));
     }
 }
