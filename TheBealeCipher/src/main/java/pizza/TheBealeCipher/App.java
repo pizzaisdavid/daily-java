7b9ecd8fb0c76be3861f971fa8c54cb369ba05de
David Germain <davidispizza@gmail.com>
2016-07-10 15:29:07 -0400

    Pass the first basic test

--- TheBealeCipher/src/main/java/pizza/TheBealeCipher/App.java
+++ TheBealeCipher/src/main/java/pizza/TheBealeCipher/App.java
@@ -1,9 +1,22 @@
 package pizza.TheBealeCipher;
 
+import java.util.ArrayList;
+
+import com.pizzaisdavid.JunitHelper.JunitHelper;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	
+  public static void main( String[] args )
+  {
+  	System.out.println( "Hello World!" );
+  }
+  
+	public static ArrayList<String> bealeCipher(ArrayList<String> message, ArrayList<Integer> key) {
+		ArrayList<String> output = new ArrayList<String>();
+		for (int value : key) {
+			output.add(message.get(value - 1));
+		}
+		return output;
+	}
 }
--- TheBealeCipher/src/test/java/pizza/TheBealeCipher/AppTest.java
+++ TheBealeCipher/src/test/java/pizza/TheBealeCipher/AppTest.java
@@ -2,13 +2,19 @@ package pizza.TheBealeCipher;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import com.pizzaisdavid.JunitHelper.JunitHelper;
 
 public class AppTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void BealeCipher_inOrder() {
+		ArrayList<String> message = JunitHelper.arrayList.make("This", "is", "a", "test");
+		ArrayList<Integer> key = JunitHelper.arrayList.make(1, 2, 3, 4);
+		ArrayList<String> output = JunitHelper.arrayList.make("This", "is", "a", "test");
+		assertEquals(output, App.bealeCipher(message, key));
 	}
-
 }
