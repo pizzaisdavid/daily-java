0ec66c6e52ce2a62844497e958d4cdbc490e5468
David Germain <davidispizza@gmail.com>
2016-07-10 15:03:16 -0400

    Init

---
+++ TheBealeCipher/src/main/java/pizza/TheBealeCipher/App.java
@@ -0,0 +1,13 @@
+package pizza.TheBealeCipher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
