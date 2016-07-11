149a328c647a452c08ff668020736a0312bdd152
David Germain <davidispizza@gmail.com>
2016-07-10 23:26:13 -0400

    Add parsing from file

--- TheBealeCipher/src/main/java/pizza/TheBealeCipher/App.java
+++ TheBealeCipher/src/main/java/pizza/TheBealeCipher/App.java
@@ -2,14 +2,34 @@ package pizza.TheBealeCipher;
 
 import java.util.ArrayList;
 
+import main.java.com.mycompany.daily.Parser;
+import main.java.com.mycompany.daily.TextFile;
+
+
 public class App 
 {
 	
-  public static void main( String[] args )
-  {
-  	System.out.println( "Hello World!" );
+  public static void main( String[] args ) {
+  	ArrayList<String> words = parseWordsFromFile();
+  	ArrayList<Integer> key = parseKeyFromFile();
+  	System.out.println(words);
+  	System.out.print(key);
   }
-  
+
+	private static ArrayList<String> parseWordsFromFile() {
+  	TextFile file = new TextFile("words.txt");
+  	ArrayList<String> lines = file.getLines();
+  	String content = lines.get(0);
+  	return Parser.parse(content, " ");
+	}
+	
+	private static ArrayList<Integer> parseKeyFromFile() {
+  	TextFile file = new TextFile("key.txt");
+  	ArrayList<String> lines = file.getLines();
+  	String content = lines.get(0);
+  	return Parser.integersFromList(content);  	
+	}
+
 	public static ArrayList<String> bealeCipher(ArrayList<String> message, ArrayList<Integer> key) {
 		ArrayList<String> output = new ArrayList<String>();
 		for (int value : key) {
