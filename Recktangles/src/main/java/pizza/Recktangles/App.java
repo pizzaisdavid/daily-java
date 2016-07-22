363d91e58fe8968539677998a4f6666129083722
David Germain <davidispizza@gmail.com>
2016-07-22 17:47:00 -0400

    Make first failing test

--- Recktangles/src/main/java/pizza/Recktangles/App.java
+++ Recktangles/src/main/java/pizza/Recktangles/App.java
@@ -1,9 +1,14 @@
 package pizza.Recktangles;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.ArrayList;
+
+public class App {
+	public static void main( String[] args ) {
+		System.out.println( "Hello World!" );
+		}
+	
+	public static ArrayList<ArrayList<String>> makeSquare(String word, int width, int height) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Recktangles/src/test/java/pizza/Recktangles/AppTest.java
+++ Recktangles/src/test/java/pizza/Recktangles/AppTest.java
@@ -1,14 +1,39 @@
 package pizza.Recktangles;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.pizzaisdavid.JunitHelper.JunitHelper;
+
 public class AppTest {
+	String word;
+	int width;
+	int height;
+	ArrayList<ArrayList<String>> expected;
+	ArrayList<ArrayList<String>> actual;
+	
+	@Before
+	public void before() {
+		
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		word = "REKT";
+		width = 1;
+		height = 1;
+    expected = JunitHelper.arrayList.make(
+      new String[] {"R","E", "K", "T"},
+      new String[] {"E"," ", " ", "K"},
+      new String[] {"K"," ", " ", "E"},
+      new String[] {"T","K", "E", "R"}
+    );
+    actual = App.makeSquare(word, width, height);
+    assertEquals(expected, actual);
 	}
 
 }
