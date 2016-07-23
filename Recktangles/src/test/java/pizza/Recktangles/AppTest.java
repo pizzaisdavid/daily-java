34f63ea53d884cdcd1dc8fcc37ad67be2cc0db68
David Germain <davidispizza@gmail.com>
2016-07-23 16:04:38 -0400

    More tests

--- Recktangles/src/test/java/pizza/Recktangles/AppTest.java
+++ Recktangles/src/test/java/pizza/Recktangles/AppTest.java
@@ -13,27 +13,82 @@ public class AppTest {
 	String word;
 	int width;
 	int height;
-	ArrayList<ArrayList<String>> expected;
 	ArrayList<ArrayList<String>> actual;
 	
 	@Before
 	public void before() {
 		
 	}
-
+	
 	@Test
-	public void test() {
+	public void calculateCharacters() {
+		word = "REKT";
+		width = 2;
+		height = 3;
+    Square square = new Square(word, width, height);
+    square.calculateSquareDimensions();
+    assertEquals(7, square.numberOfCharactersWidth);
+    assertEquals(10, square.numberOfCharactersHeight);
+	}
+	
+	@Test
+	public void createFirstRow() {
 		word = "REKT";
 		width = 1;
 		height = 1;
-    expected = JunitHelper.arrayList.make(
-      new String[] {"R","E", "K", "T"},
+    ArrayList<String> expected = JunitHelper.arrayList.make("R","E", "K", "T");
+    Square square = new Square(word, width, height);
+    actual = square.parse();
+    assertEquals(expected, actual.get(0));
+	}
+	
+	@Test
+	public void createSecondRow() {
+		word = "REKT";
+		width = 1;
+		height = 1;
+    ArrayList<String> expected = JunitHelper.arrayList.make("E"," ", " ", "K");
+    Square square = new Square(word, width, height);
+    actual = square.parse();
+    assertEquals(expected, actual.get(1));
+	}
+
+
+	@Test
+	public void createSmallSquare() {
+		word = "REKT";
+		width = 1;
+		height = 1;
+		ArrayList<ArrayList<String>> expected = JunitHelper.arrayList.make(
+      new String[] {"T","K", "E", "R"},
       new String[] {"E"," ", " ", "K"},
       new String[] {"K"," ", " ", "E"},
       new String[] {"T","K", "E", "R"}
     );
-    actual = App.makeSquare(word, width, height);
+    Square square = new Square(word, width, height);
+    actual = square.parse();
     assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void createMediumSquare() {
+		word = "REKT";
+		width = 1;
+		height = 1;
+		ArrayList<ArrayList<String>> expected = JunitHelper.arrayList.make(
+      new String[] {"T","K", "E", "R", "E", "K", "T"},
+      new String[] {"K"," ", " ", "E", " ", " ", "K"},
+      new String[] {"E"," ", " ", "K", " ", " ", "E"},
+      new String[] {"R","E", "K", "T", "K", "E", "R"},
+      new String[] {"K"," ", " ", "E", " ", " ", "K"},
+      new String[] {"E"," ", " ", "K", " ", " ", "E"},
+      new String[] {"T","K", "E", "R", "E", "K", "T"}
+    );
+    Square square = new Square(word, width, height);
+    actual = square.parse();
+    assertEquals(expected, actual);
+	}
+
+
 
 }
