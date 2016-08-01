fc7775b9d754dc1b5293553badabac505eb1fbb3
David Germain <davidispizza@gmail.com>
2016-08-01 18:14:52 -0400

    Init

---
+++ SimplifyingFractions/src/main/java/pizza/SimplifyingFractions/App.java
@@ -0,0 +1,13 @@
+package pizza.SimplifyingFractions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
