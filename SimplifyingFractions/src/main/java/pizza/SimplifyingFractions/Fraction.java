3e5250a220c38ebdd98cab8410e2e2378b2e785e
David Germain <davidispizza@gmail.com>
2016-08-05 22:16:21 -0400

    Add large test

--- SimplifyingFractions/src/main/java/pizza/SimplifyingFractions/Fraction.java
+++ SimplifyingFractions/src/main/java/pizza/SimplifyingFractions/Fraction.java
@@ -10,6 +10,21 @@ public class Fraction {
 	}
 
 	public void simplify() {
+		int largerNumber = max(numerator, denominator);
+		for (int i = largerNumber; i != 0; i--) {
+			if (numerator % i == 0 && denominator % i == 0) {
+				numerator = numerator / i;
+				denominator = denominator / i;
+				break;
+			}
+		}
+	}
+
+	private int max(int number1, int number2) {
+		if (number1 < number2) {
+			return number2;
+		}
+		return number1;
 	}
 
 	public int getNumerator() {
--- SimplifyingFractions/src/test/java/pizza/SimplifyingFractions/FractionTest.java
+++ SimplifyingFractions/src/test/java/pizza/SimplifyingFractions/FractionTest.java
@@ -7,13 +7,23 @@ import org.junit.Test;
 public class FractionTest {
 
 	@Test
-	public void test() {
+	public void smallNumberTest() {
 		Fraction fraction = new Fraction(4, 8);
 		fraction.simplify();
 		int numerator = fraction.getNumerator();
 		int denominator = fraction.getDenominator();
 		assertEquals(1, numerator);
 		assertEquals(2, denominator);
+	}
+	
+	@Test
+	public void largeNumberTest() {
+		Fraction fraction = new Fraction(1536, 78360);
+		fraction.simplify();
+		int numerator = fraction.getNumerator();
+		int denominator = fraction.getDenominator();
+		assertEquals(64, numerator);
+		assertEquals(3265, denominator);
 	}
 
 }
