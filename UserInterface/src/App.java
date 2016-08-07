81dc249dd179ddd38f62eefe6792c4d99de511a2
David Germain <davidispizza@gmail.com>
2016-08-07 13:03:41 -0400

    Add basic input grabbing

--- UserInterface/src/App.java
+++ UserInterface/src/App.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class App {
+	
+  public static void main( String[] args ) {
+  	int integer = askInteger("Please enter an integer: ");
+  	System.out.println(integer);
+  }
+
+	private static int askInteger(String prompt) {
+		Scanner reader = makeInputScanner();
+		System.out.println(prompt);
+		int n = reader.nextInt();
+		return n;
+	}
+
+	private static Scanner makeInputScanner() {
+		return new Scanner(System.in);
+	}
 
 }
