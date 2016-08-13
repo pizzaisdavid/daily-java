2053cab1ae114b5fca4f9c70e5f9df3a4a32d0e9
David Germain <davidispizza@gmail.com>
2016-08-13 14:49:41 -0400

    Add unitt testing

---
+++ GettingADegree/src/test/java/pizza/GettingADegree/ConverterTest.java
@@ -0,0 +1,14 @@
+package pizza.GettingADegree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConverterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
